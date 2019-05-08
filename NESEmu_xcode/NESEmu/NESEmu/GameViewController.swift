6ee10291d6c942e5ab8377de450d28840bfc7346
Ethan Williams <ewilli23@nd.edu>
2019-05-08 02:44:49 -0400

    File Open dialog and emulator init

---
+++ NESEmu_xcode/NESEmu/NESEmu/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  NESEmu
+//
+//  Created by Ethan Williams on 5/8/19.
+//  Copyright © 2019 NESEmu. All rights reserved.
+//
+
+import Cocoa
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
